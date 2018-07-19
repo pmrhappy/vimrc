2d0f9347169c82113656cd2e9006591e3e62f7e2
Mush <pmrhappy@gmail.com>
2018-07-19 09:32:09 +0000

    add install_dep_tools for all plugins install

--- scripts/vim_plugins/ycm.sh
+++ scripts/vim_plugins/ycm.sh
@@ -1,5 +1,4 @@
 sudo apt-get install build-essential cmake
 sudo apt-get install build-essential cmake3
 sudo apt-get install python-dev python3-dev
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
+python `find ~/.vim -name "install.py" | grep YouCompleteMe` --clang-completer
